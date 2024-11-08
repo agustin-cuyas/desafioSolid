83919ca1791188775c71bb35200a7c6f704de47d
agustin-cuyas <agustin.cuyas@moxit.com.ar>
2024-11-08 16:17:04 -0300

    Actualización planteo ISP

--- ISP/clases.hpp
+++ ISP/clases.hpp
@@ -1,5 +1,148 @@
+/*
+Agregar clase Remolque que herede vehiculo, movil y llenar.
+atributos: bool esCerrado
+
+IMovil que imprima en movimiento
+IMantenimiento atributo bool necesitaMantenimiento y hacer similar a los vehiculos electricos que al cargar se pedía una caracteristica mas. método service cambia esta variable de ser necesario
+ILlenar atributo capacidadCarga (sacarsela a camion y poner en esta, total la hereda). método llenar que pida cantidad de cajas
+
+Constructores para estas clases, ver el de electricidad
+
+*/
 #ifndef CLASES_HPP
 #define CLASES_HPP
 
+#include <iostream>
+using namespace std;
+
+class Vehiculo{
+    public:
+        Vehiculo(const string& marca, const string& modelo, const int anio, double kilometraje);
+        virtual ~Vehiculo() = default; // Destructor virtual para clase polimórfica
+
+        void setMarca(const string&);
+        string getMarca() const;
+
+        void setModelo(const string&);
+        string getModelo() const;
+
+        void setAnio(const int);
+        int getAnio() const;
+
+        void setKilometraje(double);
+        double getKilometraje();
+
+    protected: 
+        string marca;
+        string modelo;
+        int anio;
+        double kilometraje;
+};
+
+class IMovil{
+    public:
+        void mover();
+};
+
+class IMantenimiento{
+    public:
+        void service();
+};
+
+class ILlenar{
+    public:
+        void llenar();
+        
+        void setCapacidad(const double);
+        int getCapacidad() const;
+
+    protected:
+        double capacidadCarga;
+};
+
+class Auto : public Vehiculo, public IMovil, public IMantenimiento{
+    public:
+        Auto(const string& marca, const string& modelo, const int anio, double kilometraje, const int cantidadPuertas);
+        
+        void setPuertas(const int);
+        int getPuertas() const;
+
+    protected:
+        int cantidadPuertas;    
+};
+
+class Moto : public Vehiculo, public IMovil, public IMantenimiento{
+    public:
+        Moto(const string& marca, const string& modelo, const int anio, double kilometraje, const bool tieneSidecar);
+
+        void setSidecar(const char);
+        bool getSidecar() const;
+
+    protected:
+        bool tieneSidecar;
+};
+
+class Camion : public Vehiculo, public IMovil, public IMantenimiento, public ILlenar{
+    public:
+        Camion(const string& marca, const string& modelo, const int anio, double kilometraje, const double capacidadCarga);
+};
+
+
+class VehiculoCargar{
+    public:
+        virtual void ingresarInfo(Vehiculo&) = 0;
+
+    protected:
+        string marca;
+        string modelo;
+        int anio;
+        double km;
+};
+
+
+class AutoCargar : public VehiculoCargar{
+    public:
+        AutoCargar() = default;
+        void ingresarInfo(Vehiculo&) override;
+
+    private:
+        int cantidadPuertas;  
+};
+
+class MotoCargar : public VehiculoCargar{
+    public:
+        void ingresarInfo(Vehiculo&) override;
+
+    private:
+        char tieneSidecar;  
+};
+
+class CamionCargar : public VehiculoCargar{
+    public:
+        void ingresarInfo(Vehiculo&) override;
+    private:
+        double capacidadCarga;
+};
+
+
+class VehiculoMostrar{
+    public:
+        virtual void mostrarInfo(Vehiculo&) = 0;
+};
+
+class AutoMostrar : public VehiculoMostrar{
+    public:
+        void mostrarInfo(Vehiculo&) override;
+};
+
+class MotoMostrar : public VehiculoMostrar{
+    public:
+        void mostrarInfo(Vehiculo&) override;
+};
+
+class CamionMostrar : public VehiculoMostrar{
+    public:
+        void mostrarInfo(Vehiculo&) override;
+};
 
 #endif
--- ISP/main.cpp
+++ ISP/main.cpp
@@ -2,4 +2,150 @@
 Segregación de Interfaces: Los clientes no deben verse obligados a depender de interfaces que no utilizan.
 Ejemplo: Crear interfaces pequeñas y específicas como IMovil para métodos de movimiento y IMantenimiento para métodos de mantenimiento, en lugar de una interfaz única IVehiculo que incluya todo. Así, Automovil podría implementar ambas, pero Remolque podría implementar solo IMovil.
 */
+
+/*
+Hacer similar a cuando revisaba si el vehiculo era electrico para cargar, que en este caso si se puede hacer mantenimiento o llenar. Todos pueden ser conducidos, solucionar eso
+*/
+
 #include "mainHeader.hpp"
+
+enum Opcion{
+    agregar = 1,
+    mostrar,
+    salir
+};
+
+enum enumVehiculo   //si pongo Vehiculo lo confunde con la clase
+{
+    enumauto = 1,   //si pongo auto lo confunde con el tipo de dato
+    moto,
+    camion
+};
+
+void moverVehiculo(Vehiculo*);
+
+void agregarVehiculo(vector<Vehiculo*>& flota) {
+    int tipo;
+    char esElectrico;
+
+    cout << "\nIngrese el tipo de vehiculo:\n";
+    cout << "1. Auto\n";
+    cout << "2. Moto\n";
+    cout << "3. Camion\n";
+    cout << "Elija una opcion: ";
+    cin >> tipo;
+
+    Vehiculo* vehiculo = nullptr; // variable temporal para crear y configurar un nuevo vehiculo
+    VehiculoCargar* cargarvehiculo = nullptr; 
+
+    switch (tipo) {
+        case enumVehiculo::enumauto:
+            vehiculo = new Auto("", "", 0, 0.0, 0);
+            cargarvehiculo = new AutoCargar();
+            break;
+        case enumVehiculo::moto:
+            vehiculo = new Moto("", "", 0, 0.0, false);
+            cargarvehiculo = new MotoCargar();
+            break;
+        case enumVehiculo::camion:
+            vehiculo = new Camion("", "", 0, 0.0, 0.0);
+            cargarvehiculo = new CamionCargar();
+            break;        
+        default:
+            cout << "Opcion invalida.\n";
+            return;
+    }
+
+    cargarvehiculo->ingresarInfo(*vehiculo);
+    flota.push_back(vehiculo);
+    cout << "Vehiculo agregado exitosamente.\n";
+}
+
+void mostrarVehiculos(const vector<Vehiculo*>& flota){
+
+    if(flota.size()==0)
+        cout << "\nNo tiene vehiculos cargados." << endl;
+    else
+    {   
+        VehiculoMostrar* mostrar;
+        for (size_t i = 0; i < flota.size(); ++i) {    //size() devuelve una variable type_t, por eso defino i asi
+            cout << "\nVehiculo " << i + 1 << ":\n";
+            Vehiculo* vehiculo = flota[i];
+            
+            if (auto* car = dynamic_cast<Auto*>(vehiculo)) {
+                AutoMostrar mostrar;
+                mostrar.mostrarInfo(*car);
+            } 
+            else if (auto* motoPtr = dynamic_cast<Moto*>(vehiculo)) {
+                MotoMostrar mostrar;
+                mostrar.mostrarInfo(*motoPtr);
+            } 
+            else if (auto* camionPtr = dynamic_cast<Camion*>(vehiculo)) {
+                CamionMostrar mostrar;
+                mostrar.mostrarInfo(*camionPtr);
+            }
+        }
+
+        //dynamic_cast a las interfaces
+
+        char mover;
+        bool quiereMoverse = false;
+        do
+        {   cout << "\nDesea conducir algún vehículo? (S/N): ";
+            cin >> mover;
+            tolower(mover);
+            
+            if(mover == 's'){
+                quiereMoverse = true;
+
+                int indice;
+                do{
+                    cout << "Ingrese el numero de vehiculo: ";
+                    cin >> indice;
+                }while(indice<1 || indice > flota.size());
+
+                Vehiculo* vehiculo = flota[indice - 1];
+                moverVehiculo(vehiculo);
+            }
+
+            else if(mover == 'n')
+                quiereMoverse = false;
+
+        }while(quiereMoverse);
+
+    }
+}
+
+int main(){
+    vector<Vehiculo*> flota; //coleccion que mantiene todos los vehiculos creados
+    int opcion;    
+
+    do {
+            cout << "\nMenu Principal:\n";
+            cout << "1. Agregar vehiculo\n";
+            cout << "2. Mostrar vehiculos\n";
+            cout << "3. Salir\n";
+            cout << "Elija una opcion: ";
+            cin >> opcion;
+
+            switch (opcion) {
+                case Opcion::agregar:
+                    agregarVehiculo(flota);
+                    break;
+                case Opcion::mostrar:
+                    mostrarVehiculos(flota);
+                    break;
+                case Opcion::salir:
+                    cout << "Saliendo...\n";
+                    break;
+                default:
+                    cout << "Opcion invalida. Intente nuevamente.\n";
+            }
+        } while (opcion != 3);
+
+        for (auto& vehiculo : flota) {  
+            delete vehiculo;
+        }
+
+    return 0;
+}
